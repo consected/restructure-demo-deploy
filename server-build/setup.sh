5cab7e327858e53fa1fddc68033e791ad230c38f
Phil <phil.ayres@consected.com>
2021-08-13 10:27:47 +0100

    Bugfix setup

--- server-build/setup.sh
+++ server-build/setup.sh
@@ -3,6 +3,6 @@
 cd /root/restructure-demo-deploy
 SETUP_DIR=/root/setup
 mkdir -p ${SETUP_DIR}
-cp -r server-build ${SETUP_DIR}
+cp -r server-build/* ${SETUP_DIR}/
 mv ${SETUP_DIR}/scripts/* ${SETUP_DIR}/
 
