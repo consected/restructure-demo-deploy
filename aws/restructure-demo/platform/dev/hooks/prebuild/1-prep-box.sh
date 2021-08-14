d39bc25d00f0a9403a97ddf9f8d4cdd8bf737507
Phil <phil.ayres@consected.com>
2021-08-14 12:34:14 +0100

    Correct box setup

--- aws/restructure-demo/platform/dev/hooks/prebuild/1-prep-box.sh
+++ aws/restructure-demo/platform/dev/hooks/prebuild/1-prep-box.sh
@@ -5,8 +5,8 @@ BOX_NAME=restructure_demo
 # Standard
 set -xv
 echo "Starting build box for: ${BOX_NAME}"
-
+yum install -y git
 cd /root
 git clone https://github.com/consected/restructure-demo-deploy.git
-source restructure-build/server-build/setup.sh
-source setup/build_${BOX_NAME}.sh
+source restructure-demo-deploy/server-build/setup.sh
+source /root/setup/build_${BOX_NAME}.sh
