fe0f8e8ad1f0e462b1155883e21fcc94fc9fa86c
Phil <phil.ayres@consected.com>
2021-10-07 10:24:36 +0100

    Force replacement of setup files

--- server-build/setup.sh
+++ server-build/setup.sh
@@ -3,6 +3,5 @@
 cd /root/restructure-demo-deploy
 SETUP_DIR=/root/setup
 mkdir -p ${SETUP_DIR}
-cp -r server-build/* ${SETUP_DIR}/
-mv ${SETUP_DIR}/scripts/* ${SETUP_DIR}/
-
+\cp -fr server-build/* ${SETUP_DIR}/
+mv -f ${SETUP_DIR}/scripts/* ${SETUP_DIR}/
