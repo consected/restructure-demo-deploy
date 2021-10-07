1f706b361c87bb1dce300b9494f9e0d793c71dfd
Phil <phil.ayres@consected.com>
2021-10-07 13:28:46 +0100

    Retry to avoid race conditions

--- aws/restructure-demo/platform/dev/hooks/prebuild/1-prep-box.sh
+++ aws/restructure-demo/platform/dev/hooks/prebuild/1-prep-box.sh
@@ -5,8 +5,20 @@ BOX_NAME=restructure_demo
 # Standard
 set -xv
 echo "Starting build box for: ${BOX_NAME}"
+
 yum install -y git
+while [ $? != 0 ]; do
+  sleep 10
+  yum install -y git
+done
+
 cd /root
+rm -rf restructure-demo-deploy
+
 git clone https://github.com/consected/restructure-demo-deploy.git
+while [ $? != 0 ]; do
+  sleep 30
+  git clone https://github.com/consected/restructure-demo-deploy.git
+done
 source restructure-demo-deploy/server-build/setup.sh
 source /root/setup/build_${BOX_NAME}.sh
